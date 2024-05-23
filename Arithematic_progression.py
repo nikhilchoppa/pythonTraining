8f1afeb1b06d57e640df87ae312d5a25bead59ee
Shashankpedditti <shashankreddy2306@gmail.com>
2024-05-23 18:46:04 -0500

    Your commit message

--- Arithematic_progression.py
+++ Arithematic_progression.py
@@ -13,7 +13,7 @@ Input:
 Output:
 11 """
 
-print("Let's find arithematic progression using 3 numbers provided ")
+print("Let's find arithematic progression using 3 numbers")
 a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
 c=int(input("Enter third number: "))
