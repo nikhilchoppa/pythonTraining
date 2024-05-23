c38941fd5d14b1b904fd0c14076ef2af9538863f
Nikhil1612 <nikhiljiva7@gmail.com>
2024-05-23 18:07:19 -0500

    aritmetic progression done

---
+++ 23_may/arithmetic_progression.py
@@ -0,0 +1,19 @@
+'''Given first 3 number of a arthimetic progression, predict the next number.
+For details about arithmetic progression, you can visit the following link https://en.wikipedia.org/wiki/Arithmetic_progression
+Input
+3 integers, each should be taken as a input
+Output
+single integer
+Example
+Input:
+2
+5
+8
+Output:
+11
+'''
+first=int(input())
+second=int(input())
+third=int(input())
+cd=second-first
+print("next num is ",third+cd)
