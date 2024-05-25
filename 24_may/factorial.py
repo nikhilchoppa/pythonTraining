176e29b2a28c512a28a8aec905cca6efb7cb8b9d
Nikhil1612 <nikhiljiva7@gmail.com>
2024-05-25 15:02:05 -0500

    done

---
+++ 24_may/factorial.py
@@ -0,0 +1,18 @@
+'''Description
+Write a program that computes the factorial of a given integer. Your implementation should be iterative in nature. Factorial of an integer n is defined as Factorial(n) = 1*2*3*....*(n-2)*(n-1)*n. Factorial(0) is defined as 1. Factorial of a negative integer is undefined.
+Input format
+One line containing an integer n.
+Output format
+One line containing the factorial of n.
+Sample input
+5
+Sample output
+120
+'''
+a=int(input("enter number:"))
+fact=1
+i=1
+while i<=a:
+    fact=fact*i
+    i+=1
+print(fact)
