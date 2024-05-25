1b92fd781eba544d0e16267f43dc95b7d30575f4
Nikhil1612 <nikhiljiva7@gmail.com>
2024-05-25 10:50:39 -0500

    24_may on 25th

---
+++ 24_may/simple_interest.py
@@ -0,0 +1,18 @@
+
+'''Simple interest
+Simple interest formula is given by: Simple Interest = (P x T x R)/100 Where, P is the principle amount T is the time and R is the rate. Compute simple interest for given P, T and R.
+Input
+Three lines containing integer each.
+Output
+One line containing integer.
+Example
+Input:
+1000
+10
+5
+Output: 500'''
+p=int(input("principal: "))
+t=int(input("time: "))
+r=int(input("rate: "))
+si=(p*t*r)/100
+print(f"interest is :{int(si)}")
